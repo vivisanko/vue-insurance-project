3600eb3f1507be4e6aa46a5373f15ab4574962a3
vivisanko <vivisanko@gmail.com>
2017-11-05 04:14:01 +0300

    deploy file

---
+++ deploy.bat
@@ -0,0 +1,9 @@
+call npm run build
+git add .
+git commit -m "dist"
+git push origin master
+git checkout gh-pages
+git checkout master dist
+git commit -m "update dist"
+git push origin gh-pages
+git checkout master
