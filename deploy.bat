a1583032072280157578c4eecf8239a609cb30e4
vivisanko <vivisanko@gmail.com>
2017-11-05 04:11:52 +0300

    deploy update

--- deploy.bat
+++ deploy.bat
@@ -1,16 +1,9 @@
 call npm run build
 git add .
-pause
 git commit -m "dist"
-pause
 git push origin master
-pause
 git checkout gh-pages
-pause
 git checkout master dist
-pause
 git commit -m "update dist"
-pause
 git push origin gh-pages
-pause
 git checkout master
